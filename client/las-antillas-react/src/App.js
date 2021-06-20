1fc90e1941c34d372878a993ad49dde038eae17c
gabrielpasquantonio <64622616+gabrielpasquantonio@users.noreply.github.com>
2021-06-20 14:56:14 -0300

    creating back routes

--- client/las-antillas-react/src/App.js
+++ client/las-antillas-react/src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
-import Header from './components/header'
-import Home from './pages/Home'
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/header";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+
 function App() {
   return (
     <div className="App">
-    <Header/>
-   <Home/>
+      <Router>
+        <Switch>
+        
+          <Route path="/login">
+          <Header />
+            <LogIn />
+          </Route>
+          <Route path="/home">
+          <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/las-antillas-react/src/pages/LogIn.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/header'
+
+
+function LogIn() {
+    return (
+        <div>
+          <Header/>  
+        </div>
+    )
+}
+
+export default LogIn
